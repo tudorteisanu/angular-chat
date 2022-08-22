be1bc9ebf4d3d176824a04e8022df2942612282a
Tudor Teisanu <tudor.teisanu@powerit.dev>
2022-08-23 01:17:22 +0300

    add production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
+const apiBaseUrl = 'https://chat-api.nanoit.dev';
+
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:4000/api',
-  apiWsUrl: 'http://localhost:4000',
+  apiUrl: `${apiBaseUrl}/api`,
+  apiWsUrl: apiBaseUrl,
 };
